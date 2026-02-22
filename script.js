c89ac155942bbd89090ff01094396d89cebfb9e7
pavankumar <bhopallypavankumar@gmail.com>
2026-02-22 15:12:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// let programming =["java","python","javascript","c++",]
+
+// for(let i = 1;i<programming.length;i++){
+//     console.log(programming[i]);
+//     for(let j = 1;j<=10;j++){
+//         console.log("inner loop"+j);
+//     }
+// } 
+
+// let student = {
+//     first_name: "pavan",
+//     last_name: "kumar",
+//     age: 20,
+//     couse:"CSIT"
+
+
+// }
+// console.log(student);
+let addTwoNum = (a,b) =>{
+    return a+b;
+} 
+let ans = addTwoNum(3,5);
+console.log(ans);
+console.log(addTwoNum(4,6));
